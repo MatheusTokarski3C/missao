3c070f6feabae13391f05bd0e60c7b5201476667
MatheusTokarski3C <159448368+MatheusTokarski3C@users.noreply.github.com>
2024-06-06 10:39:25 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principa");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativa = document.querySelector("caixa-alternativa");
+const caixaResultados = document.querySelector("caixa-resultados");
+const caixaResultados = document.querySelector("caixa-resultados");
+
+const perguntas = [
+    {//primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+        alternativa : ["alternativa1", "alternativa2"]
+    },
+    {//primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+        alternativa : ["alternativa1", "alternativa2"]
+    }
+]
